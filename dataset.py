a8e52355eae702408efe558fd94fec4cf5ef07a2
Mohammadreza Ghofrani <ghofrani132@gmail.com>
2023-07-29 19:29:51 +0000

    Add raw multiwoz processing

--- dataset.py
+++ dataset.py
@@ -8,10 +8,12 @@ class SCGPTDatasetForTrain(Dataset):
         self,
         tokenizer,
         dataset_path,
+        dataset_type,
         max_length
     ):
         self.tokenizer = tokenizer
         self.max_length = max_length
+        self.dataset_type = dataset_type
         self.dataset = self.read_dataset(dataset_path)
         self.dataset = self.prepare_dataset()
 
@@ -22,24 +24,35 @@ class SCGPTDatasetForTrain(Dataset):
 
     def prepare_dataset(self):
         dataset = list()
-        for _, dialogue in tqdm(self.dataset.items()):
-            for turn in dialogue['log']:
-                sys_act = turn['sys_act']
-                resp_delex = turn['resp_delex']
+        if self.dataset_type == 'damd':
+            for _, dialogue in tqdm(self.dataset.items()):
+                for turn in dialogue['log']:
+                    dataset.append((
+                        turn['sys_act'],
+                        turn['resp_delex']
+                    ))
+        elif self.dataset_type == 'raw':
+            for entry in tqdm(self.dataset):
+                dataset.append((
+                    entry['sys_act'],
+                    entry['resp']
+                ))
 
-                text = sys_act + self.tokenizer.bos_token + resp_delex + self.tokenizer.eos_token
-                text_tokenized = self.tokenizer(text, padding='max_length', return_tensors='pt',
-                                                max_length=self.max_length)
+        tokenized_dataset = list()
+        for sys_act, resp in dataset:
+            text = sys_act + self.tokenizer.bos_token + resp + self.tokenizer.eos_token
+            text_tokenized = self.tokenizer(text, padding='max_length', return_tensors='pt',
+                                            max_length=self.max_length)
 
-                input_ids = text_tokenized['input_ids'].clone().detach().squeeze()
-                attention_mask = text_tokenized['attention_mask'].clone().detach().squeeze()
-                labels = text_tokenized['input_ids'].clone().detach().squeeze()
+            input_ids = text_tokenized['input_ids'].clone().detach().squeeze()
+            attention_mask = text_tokenized['attention_mask'].clone().detach().squeeze()
+            labels = text_tokenized['input_ids'].clone().detach().squeeze()
 
-                start_of_resp_index = (input_ids == self.tokenizer.bos_token_id).nonzero(as_tuple=True)[0].item() + 1
-                labels[:start_of_resp_index] = -100
+            start_of_resp_index = (input_ids == self.tokenizer.bos_token_id).nonzero(as_tuple=True)[0].item() + 1
+            labels[:start_of_resp_index] = -100
 
-                dataset.append((input_ids, attention_mask, labels))
-        return dataset
+            tokenized_dataset.append((input_ids, attention_mask, labels))
+        return tokenized_dataset
 
     def __len__(self):
         return len(self.dataset)
